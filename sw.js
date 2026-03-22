04c340b0fd23f6bfd7b1bb65c39c7314d3dc9b56
mhrdkaa <putramahardika276@gmail.com>
2026-03-22 14:15:18 +0800

    cape

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
+self.addEventListener('install', (event) => {
+    console.log("SW: Menginstal...");
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log("SW: Aktif dan mengambil alih!");
+    event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
-    if (event.request.url.includes('/trigger-bypass')) {
-        console.log("SW: Intercepting request, synthesizing download...");
-        const fileContent = "Ini file hasil bypass Service Worker!";
+    if (event.request.url.includes('trigger-bypass')) {
+        console.log("SW: BOOM! Request dicegat. Menyuntikkan file download...");
+
+        const fileContent = "Ini file hasil bypass Service Worker di GitHub Pages!";
         const blob = new Blob([fileContent], { type: 'application/octet-stream' });
+
         const response = new Response(blob, {
             headers: {
-                'Content-Disposition': 'attachment; filename="sw_bypass.txt"',
+                'Content-Disposition': 'attachment; filename="sw_pwned.txt"',
                 'Content-Type': 'application/octet-stream'
             }
         });
